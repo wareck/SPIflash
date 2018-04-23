a00f6c552fa4c1ab5cff2ad6a63ce001225c701d
Your Name <you@example.com>
2018-04-23 18:54:22 +0000

    fix

--- make_backup.sh
+++ make_backup.sh
@@ -2,6 +2,7 @@
 device=raspberry #arduino or raspberry
 port=ttyACM0
 baud=115200
+rpi_speed=20000 # between 512 to 30000, 20000 seems to be the best
 
 if [ $device = "arduino" ]
 then
@@ -11,7 +12,8 @@ if [ $device = "raspberry" ]
 then
 modprobe spi_bcm2835
 modprobe spidev
-flashrom -p linux_spi:dev=/dev/spidev0.0,spispeed=1000 -r backup.hex
+flashrom -p linux_spi:dev=/dev/spidev0.0,spispeed=$rpi_speed -r backup.hex
 fi
 
 echo "n'oubliez pas de renomer le fichier !!!!"
+echo "d'ont forget to rename your file !!!!"
